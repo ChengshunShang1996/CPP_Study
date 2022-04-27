496d27d3f0723687b6e12889c8052ca67dac3aae
Chengshun Shang <chengshun.shang1996@gmail.com>
2022-04-27 12:20:02 +0200

    update

--- ContactList/ContactList.cpp
+++ ContactList/ContactList.cpp
@@ -3,32 +3,32 @@
 
 void showMenu(){
 
-    std::cout << "***************************" << std::endl;
-    std::cout << "*****  1、添加联系人  *****" << std::endl;
-	std::cout << "*****  2、显示联系人  *****" << std::endl;
-	std::cout << "*****  3、删除联系人  *****" << std::endl;
-	std::cout << "*****  4、查找联系人  *****" << std::endl;
-	std::cout << "*****  5、修改联系人  *****" << std::endl;
-	std::cout << "*****  6、清空联系人  *****" << std::endl;
-	std::cout << "*****  0、退出通讯录  *****" << std::endl;
-	std::cout << "***************************" << std::endl;
+    std::cout << "*******************************" << std::endl;
+    std::cout << "*****  1. Add new person  *****" << std::endl;
+	std::cout << "*****  2. Show persons    *****" << std::endl;
+	std::cout << "*****  3. Delete persons  *****" << std::endl;
+	std::cout << "*****  4. Find persons    *****" << std::endl;
+	std::cout << "*****  5. Modify persons  *****" << std::endl;
+	std::cout << "*****  6. Clear persons   *****" << std::endl;
+	std::cout << "*****  0. Exit            *****" << std::endl;
+	std::cout << "*******************************" << std::endl;
 }
 
 void addPerson(Addressbooks *abs){
 
     if(abs->m_Size == MAX){
-        std::cout << "通讯录已满，请寻找下一个通讯录加入" << std::endl;
+        std::cout << "The addressbook is full!" << std::endl;
         return;
     }
     else{
         std::string name;
-        std::cout << "请输入姓名" << std::endl;
+        std::cout << "Please input the name:" << std::endl;
         std::cin >> name;
-        std::cout << "输入姓名ing" << std::endl;
+        std::cout << "Inputing name" << std::endl;
         abs->personArray[abs->m_Size]->m_Name = name;
-        std::cout << "输入姓名后" << std::endl;
+        std::cout << "Afetr input name" << std::endl;
 
-        std::cout << "请输入性别：" << std::endl;
+        std::cout << "Please input sexual:" << std::endl;
         std::cout << "1 -- 男" << std::endl;
         std::cout << "2 -- 女" << std::endl;
 
@@ -40,23 +40,23 @@ void addPerson(Addressbooks *abs){
                 abs->personArray[abs->m_Size]->m_Sex = sex;
                 break;
             }
-            std::cout << "输入错误，请重新输入" << std::endl;
+            std::cout << "Wrong. Please input it again" << std::endl;
         }
 
         //年龄
         int age = 0;
-        std::cout << "请输入年龄：" << std::endl;
+        std::cout << "Please input age:" << std::endl;
         std::cin >> age;
         abs->personArray[abs->m_Size]->m_Age = age;
 
         //电话
-        std::cout << "请输入电话号码：" << std::endl;
+        std::cout << "Please input phone:" << std::endl;
         std::string phone = "";
         std::cin >> phone;
         abs->personArray[abs->m_Size]->m_Phone = phone;
 
         //住址
-        std::cout << "请输入家庭住址，方便我们行窃：" << std::endl;
+        std::cout << "Please input address:" << std::endl;
         std::string address;
         std::cin >> address;
         abs->personArray[abs->m_Size]->m_Addr = address;
@@ -64,7 +64,7 @@ void addPerson(Addressbooks *abs){
         //更新通讯录人数
         abs->m_Size++;
 
-        std::cout << "添加成功" << std::endl;
+        std::cout << "Successfully added!" << std::endl;
         system("pause");
         system("cls");
     }
@@ -98,7 +98,7 @@ int main(){
             case 6:
                 break;
             case 0:
-                std::cout << "欢迎下次使用" << std::endl;
+                std::cout << "Hope to see you again!" << std::endl;
                 system("pause");
                 return 0;
                 break;
